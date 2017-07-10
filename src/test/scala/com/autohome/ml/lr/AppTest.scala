755a6d18ddbed336c04d147aea6a1b934743d39c
chenlongzhen <longzhen.chen@foxmail.com>
2017-07-10 17:32:36 +0800

    Initial commit

---
+++ src/test/scala/com/autohome/ml/lr/AppTest.scala
@@ -0,0 +1,17 @@
+package com.autohome.ml.lr
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
